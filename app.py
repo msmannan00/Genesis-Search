1ca8be745f5de0e3ddd3cd525216816b94894695
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-02-10 18:14:24 +0500

    Bug Fixes
    Bug Fixes

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from shared_directory.state_manager.constant import APP_STATUS
 # gAAAAABiBO-DDvFZzBRGjSWY_1aA8P_4w4rsI0SDKyKkSHdXtPrBYZB_NbO0UTz4ir57lNynuo-_6IrN4uySzAAluciYiPBDeXCh3TSRP0rQIwy1oCWgxJixeiYGCAajlZf3wZw1eq0Q
 
 __m_fernet = Fernet(base64.urlsafe_b64encode(str.encode(APP_STATUS.S_FERNET_KEY)))
-ss = __m_fernet.encrypt("hello".encode())
-ss = __m_fernet.decrypt("gAAAAABiBPtRG8i6gL5H7kkqNK4733Nfi6y40XVzlw1j5wLbMccX_zPhed5gbBw6p5QhhazB2UZfILTlQdofztfODmUcqkUi2gPVA_80BoM9SpVTaZMHrvPgt5aE2UcsFnM4sSHmfnun".encode())
+ss = __m_fernet.encrypt("D~S=05y68#M25oj]vprm}9HE))Tr'VX?[p|m-Wg`mrg^----1644498774".encode())
 
 print(ss.decode("utf-8"))
 
