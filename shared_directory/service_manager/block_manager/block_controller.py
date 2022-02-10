8ec3ddbfa9183f7b727292dacadf35346e42425a
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-02-10 18:07:34 +0500

    Bug Fixes
    Bug Fixes

--- shared_directory/service_manager/block_manager/block_controller.py
+++ shared_directory/service_manager/block_manager/block_controller.py
@@ -1,7 +1,11 @@
 # Local Imports
 import base64
+import math
+import time
 
 from cryptography.fernet import Fernet
+
+from modules.user_data_parser.parse_services.helper_services.helper_method import helper_method
 from shared_directory.service_manager.block_manager.block_enums import BLOCK_PARAM, BLOCK_COMMAND
 from shared_directory.service_manager.session.session_controller import session_controller
 from shared_directory.service_manager.session.session_enums import SESSION_COMMANDS
@@ -36,8 +40,15 @@ class block_controller:
             elif APP_STATUS.S_DEVELOPER is False:
                 m_secret_token = p_request.GET[BLOCK_PARAM.M_SECRET_TOKEN]
                 m_decoded_str = self.__m_fernet.decrypt(m_secret_token.encode()).decode("utf-8")
-                if m_decoded_str.startswith(APP_STATUS.S_APP_BLOCK_KEY) is True:
-                    return False
+                m_secret_key = m_decoded_str[0]
+                m_secret_time = int(m_decoded_str[1])
+
+                if m_secret_key.startswith(APP_STATUS.S_APP_BLOCK_KEY) is True:
+                    m_time = m_secret_time
+                    if abs(time.time()-m_time)>60:
+                        return True
+                    else:
+                        return False
                 return True
             return False
         except Exception as ex:
