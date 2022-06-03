9a58430f9d701af63e2c71d935210d88403c7d8c
Abdul Mannan Saeed <111820160@umt.edu.pk>
2022-06-04 03:15:23 +0500

    Bug Fixes
    Bug Fixes

--- shared_directory/state_manager/constant.py
+++ shared_directory/state_manager/constant.py
@@ -2,7 +2,7 @@ class APP_STATUS:
     S_DEVELOPER = True
     S_FERNET_KEY = "W#ZYBHQa9G_DB_iU@yjA3Es@COu4-UzU"
     S_APP_BLOCK_KEY = "D~S=05y68#M77oj]vprm}9HE))Xp'VX?[p|m-Wg`mrg^"
-    S_MAINTAINANCE = False
+    S_MAINTAINANCE = True
 
 
 class SERVER_STATUS_TYPE:
