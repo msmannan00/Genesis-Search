11761d4dee0b43431b9de78b6e48330563d5b9c4
Abdul Mannan Saeed <111820160@umt.edu.pk>
2022-11-12 09:28:57 -0800

    Update constant.py

--- shared_directory/state_manager/constant.py
+++ shared_directory/state_manager/constant.py
@@ -1,5 +1,5 @@
 class APP_STATUS:
-    S_DEVELOPER = True
+    S_DEVELOPER = False
     S_FERNET_KEY = "W#ZYBHQa9G_DB_iU@yjA3Es@COu4-UzU"
     S_APP_BLOCK_KEY = "D~S=05y68#M77oj]vprm}9HE))Xp'VX?[p|m-Wg`mrg^"
     S_MAINTAINANCE = False
