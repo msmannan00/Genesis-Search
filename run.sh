37fd8ba393a568c0f1abd6c9fd5ed6f841996776
عبدالمنان <msmannan00@protonmail.com>
2024-09-23 19:25:13 +0500

    build fixes

--- run.sh
+++ run.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+URL="https://drive.usercontent.google.com/download?id=1LTI94WsJbf8PheaMb7269Vxm5ZKtbHwb&export=download&authuser=0&confirm=t&uuid=1163171e-ce7c-4a98-9a46-2a3ce2a91f48&at=AO7h07dQiPcuFN56QrmDruowdk0P%3A1727101003781"
+DEST_DIR="static/trustly/.well-known/model"
+DEST_FILE="$DEST_DIR/model_file"
+
+mkdir -p $DEST_DIR
+
+download_file() {
+    if curl --output /dev/null --silent --head --fail "$URL"; then
+        curl -# -L "$URL" -o "$DEST_FILE"
+    else
+        echo "Error: Invalid Model URL or network issue."
+        exit 1
+    fi
+}
+
 if [ "$1" == "build" ]; then
     docker-compose down
     docker system prune -a --volumes -f
+    download_file
     docker-compose build --no-cache
     docker-compose up
 else
     docker-compose down
+    download_file
     docker-compose up
 fi
