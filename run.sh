7d57f89061bc0b6618440a156d48cd11590c3a38
عبدالمنان <msmannan00@protonmail.com>
2024-10-15 02:42:05 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-URL="https://drive.usercontent.google.com/download?id=1LTI94WsJbf8PheaMb7269Vxm5ZKtbHwb&export=download&authuser=0&confirm=t&uuid=1163171e-ce7c-4a98-9a46-2a3ce2a91f48&at=AO7h07dQiPcuFN56QrmDruowdk0P%3A1727101003781"
-DEST_DIR="static/trustly/.well-known/model"
-DEST_FILE="$DEST_DIR/toxic-model.zip"
-
-mkdir -p $DEST_DIR
-
-download_file() {
-    if [ -f "$DEST_FILE" ]; then
-        echo "File $DEST_FILE already exists. Skipping download."
-    else
-        if curl --output /dev/null --silent --head --fail "$URL"; then
-            echo "Downloading file..."
-            curl -# -L "$URL" -o "$DEST_FILE"
-        else
-            echo "Error: Invalid Model URL or network issue."
-            exit 1
-        fi
-    fi
-}
 
 remove_services() {
     read -p "Are you sure you want to remove all services? (y/n) " -n 1 -r
@@ -53,7 +34,6 @@ remove_conflicting_containers() {
 
 if [ "$1" == "build" ]; then
     remove_services
-    download_file
     remove_conflicting_containers
     docker-compose build --no-cache
     docker-compose up -d
@@ -72,7 +52,6 @@ elif [ "$1" == "reload_cache" ]; then
 else
     docker-compose down
     remove_conflicting_containers
-    download_file
     docker-compose up -d
     sleep 2
     copy_files
