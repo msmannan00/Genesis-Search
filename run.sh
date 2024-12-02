f81bfcfbf8c9ca2b543b04e251274d4eedf5dac4
عبدالمنان <msmannan00@protonmail.com>
2024-12-02 17:14:10 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 PROJECT_NAME="trusted-search"
 stop_docker() {
+    docker compose -p $PROJECT_NAME down --volumes --remove-orphans
+    docker container prune -f --filter "label=com.docker.compose.project=$PROJECT_NAME"
+    docker volume prune -f --filter "label=com.docker.compose.project=$PROJECT_NAME"
+    docker network prune -f --filter "label=com.docker.compose.project=$PROJECT_NAME"
+    docker image prune -f --filter "label=com.docker.compose.project=$PROJECT_NAME"
+    docker compose -p $PROJECT_NAME exec -T worker celery -A crawler.crawler_services.celery_manager control purge || true
+    docker compose -p $PROJECT_NAME exec -T worker celery -A crawler.crawler_services.celery_manager control revoke --terminate --all || true
+    docker compose -p $PROJECT_NAME exec -T redis redis-cli FLUSHALL || true
+
     docker compose stop
     docker cp static/. trusted-web-main:/app/static/
     docker cp trustly/templates/. trusted-web-main:/app/trustly/templates/
