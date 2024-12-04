315e2c45aeb6cf5c5be59bf1f7da0cb50cb088a7
عبدالمنان <msmannan00@protonmail.com>
2024-12-05 03:07:17 +0500

    Update jquery.init.js

--- static/admin/js/jquery.init.js
+++ static/admin/js/jquery.init.js
@@ -1,4 +1,3 @@
-/*global jQuery:false*/
 'use strict';
 /* Puts the included jQuery into our own namespace using noConflict and passing
  * it 'true'. This ensures that the included jQuery doesn't pollute the global
