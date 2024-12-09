42d5b13b72cda92ed6477688f9d02d2ad5e27ad3
عبدالمنان <msmannan00@protonmail.com>
2024-12-10 04:26:19 +0500

    js removed

--- static/trustly/css/pages/cms/blades/shared/helper_javascript/js-admin_enum.js
+++
@@ -1,8 +0,0 @@
-const DOM = {
-  S_RESULT_CONTAINER : 'm_result_success',
-  S_JAVA_ERROR : 'p_java_error',
-  S_DISPLAY_SEARCH : 'm_result_success_display',
-};
-const COMMANDS = {
-  S_RAW_JSON : 'm_raw_json_command',
-};
