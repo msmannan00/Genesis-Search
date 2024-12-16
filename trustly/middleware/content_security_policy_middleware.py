e7a4254593a933f3fcad2c99fd918cfd555d34de
عبدالمنان <msmannan00@protonmail.com>
2024-12-17 02:59:59 +0500

    Update content_security_policy_middleware.py

--- trustly/middleware/content_security_policy_middleware.py
+++ trustly/middleware/content_security_policy_middleware.py
@@ -5,15 +5,19 @@ class content_security_policy_middleware(MiddlewareMixin):
     def process_response(self, request, response):
         response['Content-Security-Policy'] = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline'; "
-            "style-src 'self' 'unsafe-inline'; "
+            "script-src 'self'; "
+            "style-src 'self'; "
             "img-src 'self' data:; "
             "font-src 'self'; "
             "connect-src 'self'; "
             "media-src 'self'; "
+            "frame-src 'none'; "
             "frame-ancestors 'none'; "
             "object-src 'none'; "
             "form-action 'self'; "
-            "base-uri 'none'; "
+            "base-uri 'self'; "
+            "upgrade-insecure-requests; "
+            "report-uri /csp-report-endpoint/; "
+            "report-to csp-endpoint;"
         )
         return response
