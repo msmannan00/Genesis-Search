ce276ae8b49d47d702022993487894b8d23fbd88
عبدالمنان <msmannan00@protonmail.com>
2024-12-18 01:23:16 +0500

    optimizations

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
+# Configuration file for Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
-
 project = 'Orion Platform Documentation'
 author = 'Your Name'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
-
-extensions = ['myst_parser']  # Enable Markdown support
-
-# Add paths for templates and static files
-templates_path = ['_templates']
-exclude_patterns = []
-
-# The suffixes of source filenames
+extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
-
-# The master toctree document
-master_doc = 'index'
+master_doc = 'index'  # Entry point for Sphinx
 
 # -- Options for HTML output -------------------------------------------------
-
-html_theme = 'sphinx_rtd_theme'  # Use Read the Docs theme
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-# -- Additional options ------------------------------------------------------
-
-# Allow Sphinx to parse Markdown files
-myst_heading_anchors = 3  # Adds anchor links to headings
